9d21ab60fec14499255d86a16c306fe390582866
Aman Thakur <amankumar3927828@gmail.com>
2025-08-01 17:49:12 +0530

    Data Types

---
+++ dataTypes.js
@@ -0,0 +1,25 @@
+"use strict"; // treat all JS code as new version
+
+// alert("hello") // we are using NodeJs not browser
+
+console.log(3 + 3); // code readability should be high
+console.log("Aman");
+
+let name = "Aman"; // string dataTypes
+let age = 22; // int dataTypes
+let isLoggedin = false; // boolean dataTypes
+
+//PRIMITIVE DATATYPES
+// number = 2 to the power 53
+// bigint = for largd numbers
+// string = "assembly of numbers"
+// boolean = true or false
+// null = stand alone value (type = object)
+// undefined = kindda placeholder for a value which is not defined yet (type = undefined)
+// symbol = uniqueness
+
+// object
+
+console.log(typeof undefined);
+console.log(typeof null);
+
